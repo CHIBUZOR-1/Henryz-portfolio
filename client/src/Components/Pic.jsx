73d76888e9b928ac617bfbb995c2fbbf26ea794f
Chibuzor Amaechi <amaechihenrychibuzor@gmail.com>
2025-04-14 04:12:41 +0100

    cicd fix

--- client/src/Components/Pic.jsx
+++ client/src/Components/Pic.jsx
@@ -1,7 +1,7 @@
 import me from '../assets/me.jpeg'
 import ecomm1 from '../assets/ecomm1.jpg'
 import zdark from '../assets/zdark.png'
-import dep from '../assets/dep.PNG'
+import dep from '../assets/dep.png'
 import porto from '../assets/porto.jpg'
 export const pix = {
     me,
